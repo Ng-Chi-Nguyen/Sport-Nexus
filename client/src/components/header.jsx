a90542bfc956d56e42b729cd43d45d773e3cb421
Ng-Chi-Nguyen <155863412+Ng-Chi-Nguyen@users.noreply.github.com>
2025-12-25 23:59:21 +0700

    Cập nhật header và search, thêm ảnh CoVNam
    Bổ sung ảnh CoVNam.jpg vào assets. Header được cập nhật với các icon mới, thêm phân cách, và thay đổi giao diện phần tài khoản và cài đặt. Phần search được cải tiến với nút 'Tìm Kiếm' và điều chỉnh chiều rộng input cho phù hợp giao diện.

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,6 +1,15 @@
-import { ChevronDown, NotebookText, ShoppingCart, User } from "lucide-react";
+import {
+  ChevronDown,
+  Languages,
+  LayoutDashboard,
+  NotebookText,
+  Settings,
+  ShoppingCart,
+  User,
+} from "lucide-react";
 import Logo from "./logo";
 import SearchHeader from "./search";
+import LogoVN from "@/assets/CoVNam.jpg";
 
 const Header = () => {
   return (
@@ -10,10 +19,11 @@ const Header = () => {
           <div className="flex-shrink-0">
             <Logo />
           </div>
+
           <div className="flex-1 max-w-2xl">
             <SearchHeader />
           </div>
-          <div className="flex items-center bg-blue-500 px-3 py-1 rounded-[30px] text-white">
+          <div className="flex items-center bg-blue-500 px-3 py-1 rounded-[30px] text-white ">
             <div className="pr-3">
               <NotebookText />
             </div>
@@ -24,7 +34,15 @@ const Header = () => {
               </span>
             </div>
           </div>
+          <span className="border border-solid w-[1px] h-8"></span>
           <div className="flex items-center">
+            <div className=" mr-3 cursor-pointer group">
+              <LayoutDashboard
+                size={28}
+                strokeWidth={1}
+                className="group-hover:text-blue-600 transition-colors"
+              />
+            </div>
             <div className="border border-solid flex items-center mr-5 px-3 py-1">
               <User className="text-blue-500" />
               <p className="text-blue-500 font-bold">Tài khoản</p>
@@ -33,9 +51,9 @@ const Header = () => {
               <ShoppingCart />
               <span className="absolute top-[3px] right-[2px] w-2 h-2 bg-red-600 rounded-[50%]"></span>
             </button>
+            <span className="border border-solid w-[1px] h-4 ml-2"></span>
             <div className="ml-2 flex items-center cursor-pointer">
-              <p>VI</p>
-              <ChevronDown size={20} strokeWidth={0.5} />
+              <Settings size={25} strokeWidth={0.6} />
             </div>
           </div>
         </div>
--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -10,8 +10,13 @@ const SearchHeader = () => {
       <input
         type="text"
         placeholder="Tìm kiếm sản phẩm"
-        className=" bg-gray-50 border border-gray-200 w-[500px] rounded-full] h-[40px] pl-10 outline-none pl-[30px] text-sm h-9 placeholder:text-gray-400"
+        className=" bg-gray-50 border border-gray-200 w-[100%] rounded-full] h-[40px] pl-10 outline-none pl-[30px] text-sm h-9 placeholder:text-gray-400"
       />
+      <div className="absolute top-[5px] right-[0] flex items-center gap-3 px-4 border-l border-gray-200 h-8 ml-4">
+        <span className="text-[12px] text-gray-500 font-medium uppercase tracking-wider">
+          Tìm Kiếm
+        </span>
+      </div>
     </div>
   );
 };
