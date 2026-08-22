7248c89335ce6c39275b030795bb2b38ee452f98
Ng-Chi-Nguyen <155863412+Ng-Chi-Nguyen@users.noreply.github.com>
2026-08-23 00:33:10 +0700

    refactor(client): extract shared downloadBlob util

--- client/src/components/admin/ExcelCrudActions.jsx
+++ client/src/components/admin/ExcelCrudActions.jsx
@@ -4,15 +4,7 @@ import { useTranslation } from "react-i18next";
 import excelCrudImportApi from "@/api/management/excelCrudImportApi";
 import ExcelCrudImportModal from "./ExcelCrudImportModal";
 import ShowToast from "@/components/ui/toast";
-
-const downloadBlob = (blob, fileName) => {
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement("a");
-  a.href = url;
-  a.download = fileName;
-  a.click();
-  window.URL.revokeObjectURL(url);
-};
+import downloadBlob from "@/utils/download.utils";
 
 const ExcelCrudActions = ({
   basePath,
---
+++ client/src/utils/download.utils.js
@@ -0,0 +1,10 @@
+const downloadBlob = (blob, fileName) => {
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = fileName;
+  a.click();
+  window.URL.revokeObjectURL(url);
+};
+
+export default downloadBlob;
